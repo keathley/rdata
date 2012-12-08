69189b70dbcc19130ac00de4ed48ff520c9c64c0
spyc3r <spyc3r@gmail.com>
2012-12-07 23:01:08 -0600

    Clean up stack formatting

--- lib/rdata/stack.rb
+++ lib/rdata/stack.rb
@@ -9,36 +9,34 @@ This is the Stack class. It allows easy implementation of stacks and use of thei
 =end
 
 module RData
-    
-    class Stack
+  class Stack
 
-        def initialize
-            @stack = Array[]
-            @top = 0
-        end
-
-        def top
-            return @stack[@top]
-        end
-
-        def push(x)
-            @top = @top + 1
-            @stack[@top] = x
-        end
-
-        def pop
-            if self.is_empty? == "true"
-                raise '[underflow] Cannot pop data from an empty stack'
-            else
-                @top = @top - 1
-                return @stack[@top + 1]
-            end 
-        end
-
-        def is_empty?
-            (@top == 0) ? 'true' : 'false'
-        end
-
+    def initialize
+      @stack = Array[]
+      @top = 0
     end
 
+    def top
+      return @stack[@top]
+    end
+
+    def push(x)
+      @top = @top + 1
+      @stack[@top] = x
+    end
+
+    def pop
+      if self.is_empty? == "true"
+          raise '[underflow] Cannot pop data from an empty stack'
+      else
+          @top = @top - 1
+          return @stack[@top + 1]
+      end 
+    end
+
+    def is_empty?
+      (@top == 0) ? 'true' : 'false'
+    end
+
+  end
 end
